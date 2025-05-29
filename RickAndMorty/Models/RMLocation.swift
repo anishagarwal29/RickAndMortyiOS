db1e4f0af2519eb014cc86ed540a256e8dd176f2
Anish Agarwal <anishagarwal.sg@gmail.com>
2025-05-29 11:44:54 +0800

    Fixed model files

--- RickAndMorty/Models/RMLocation.swift
+++ RickAndMorty/Models/RMLocation.swift
@@ -10,9 +10,9 @@ import Foundation
 struct RMLocation: Codable {
     let id: Int
     let name: String
-    let air_date: String
-    let episode: String
-    let characters: [String]
+    let type: String
+    let dimesnion: String
+    let residents: [String]
     let url: String
     let created: String
 }
